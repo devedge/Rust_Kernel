cfa1fa0afdacb45228c8709dd4d9eca06f4a7480
devedge <richard.pjski2@gmail.com>
2018-12-03 11:12:28 -0500

    print macros for VGA buffer

--- rustkernelv2/src/main.rs
+++ rustkernelv2/src/main.rs
@@ -10,16 +10,15 @@ use core::panic::PanicInfo;
 
 // Diverging function called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
   loop {}
 }
 
 // Entry point convention for Linux. Disable compiler name mangling.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  use core::fmt::Write;
-  vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-  write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+  println!("Hello World{}", "!");
 
   loop {}
 }
