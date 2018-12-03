c6b283c2e6e3d8db517b257d21aa01567e212216
devedge <richard.pjski2@gmail.com>
2018-12-03 14:52:01 -0500

    Unit tests for VGA writer

--- rustkernelv2/src/main.rs
+++ rustkernelv2/src/main.rs
@@ -1,14 +1,16 @@
 // Entry point
 
 // Override attributes first
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(unused_imports))]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 // Diverging function called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
@@ -16,6 +18,7 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 // Entry point convention for Linux. Disable compiler name mangling.
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
