684fd5fdcc39db9d1b25fff888d3468ec88c08fe
devedge <richard.pjski2@gmail.com>
2019-01-27 18:05:58 -0500

    Page Fault handler

--- rustkernelv2/src/interrupts.rs
+++ rustkernelv2/src/interrupts.rs
@@ -3,7 +3,7 @@
 #![cfg(not(windows))]
 
 use crate::{gdt, hlt_loop, print, println};
-use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame, PageFaultErrorCode};
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
 use spin;
@@ -26,6 +26,8 @@ lazy_static! {
       .set_handler_fn(timer_interrupt_handler);
     idt[usize::from(KEYBOARD_INTERRUPT_ID)]
       .set_handler_fn(keyboard_interrupt_handler);
+    idt.page_fault.set_handler_fn(page_fault_handler);
+
     idt
   };
 }
@@ -82,4 +84,17 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
   }
 
   unsafe { PICS.lock().notify_end_of_interrupt(KEYBOARD_INTERRUPT_ID) };
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+  stack_frame: &mut ExceptionStackFrame,
+  _error_code: PageFaultErrorCode,
+) {
+  use crate::hlt_loop;
+  use x86_64::registers::control::Cr2;
+
+  println!("EXCEPTION: PAGE FAULT");
+  println!("Accessed Address: {:?}", Cr2::read());
+  println!("{:?}", stack_frame);
+  hlt_loop();
 }
