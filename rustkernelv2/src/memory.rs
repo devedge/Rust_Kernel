6e768b0d63733961ff844b87448181f13110af63
devedge <richard.pjski2@gmail.com>
2019-02-13 03:35:21 -0500

    Using the RecursivePageTable type in x86 crate

--- rustkernelv2/src/memory.rs
+++ rustkernelv2/src/memory.rs
@@ -1,58 +1,27 @@
 // Paging management
 
-use x86_64::PhysAddr;
-use x86_64::structures::paging::PageTable;
+use x86_64::{VirtAddr, PhysAddr};
+use x86_64::structures::paging::{Mapper, Page, PageTable, RecursivePageTable};
 
-// Returns the physical address for a given virtual address, or 'None' if the
-// virtual address is not mapped
-pub fn translate_addr(addr: usize) -> Option<PhysAddr> {
-    // Introduce variables for the recursive index & the sign extension bits.
-    // These values should technically not be hardcoded
-    let r = 0o777; // recursive index
-    let sign = 0o177777 << 48; // sign extension
+/// Creates a RecursivePageTable instance from the level 4 address.
+///
+/// This function is unsafe because it can break memory safety if an invalid
+/// address is passed.
+pub unsafe fn init(level_4_table_addr: usize) -> RecursivePageTable<'static> {
+    let level_4_table_ptr = level_4_table_addr as *mut PageTable;
+    let level_4_table = &mut *level_4_table_ptr;
+    RecursivePageTable::new(level_4_table).unwrap()
+}
 
-    // Retrieve the page table indices of the address that we want to
-    // translate
-    let l4_idx = (addr >> 39) & 0o777; // level 4 index
-    let l3_idx = (addr >> 30) & 0o777; // level 3 index
-    let l2_idx = (addr >> 21) & 0o777; // level 2 index
-    let l1_idx = (addr >> 12) & 0o777; // level 1 index
-    let page_offset = addr & 0o7777;
+/// Returns the physical address for the given virtual address, or `None` if
+/// the virtual address is not mapped.
+pub fn translate_addr(addr: u64, recursive_page_table: &RecursivePageTable)
+    -> Option<PhysAddr>
+{
+    let addr = VirtAddr::new(addr);
+    let page: Page::containing_address(addr);
 
-    // calculate the table addresses
-    let level_4_table_addr = 
-        sign | (r << 39) | (r << 30) | (r << 21) | (r <<12);
-    let level_3_table_addr = 
-        sign | (r << 39) | (r << 30) | (r << 21) | (l4_idx <<12);
-    let level_2_table_addr = 
-        sign | (r << 39) | (r << 30) | (l4_idx << 21) | (l3_idx <<12);
-    let level_1_table_addr = 
-        sign | (r << 39) | (l4_idx << 30) | (l3_idx << 21) | (l2_idx <<12);
-
-    // check that level 4 entry is mapped
-    let level_4_table = unsafe { &*(level_4_table_addr as *const PageTable) };
-    if level_4_table[l4_idx].addr().is_null() {
-        return None;
-    }
-
-    // check that level 3 entry is mapped
-    let level_3_table = unsafe { &*(level_3_table_addr as *const PageTable) };
-    if level_3_table[l3_idx].addr().is_null() {
-        return None;
-    }
-
-    // check that level 2 entry is mapped
-    let level_2_table = unsafe { &*(level_2_table_addr as *const PageTable) };
-    if level_2_table[l2_idx].addr().is_null() {
-        return None;
-    }
-
-    // check that level 1 entry is mapped & retrieve physical address from it
-    let level_1_table = unsafe { &*(level_1_table_addr as *const PageTable) };
-    let phys_addr = level_1_table[l1_idx].addr();
-    if phys_addr.is_null() {
-        return None;
-    }
-
-    Some(phys_addr + page_offset)
+    // perform the translation
+    let frame = recursive_page_table.translate_page(page);
+    frame.map(|frame| frame.start_address() + u64::from(addr.page_offset()))
 }
