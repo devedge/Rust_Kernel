c6e0270e38b35a772e30e001d4ff9a831d8d785a
devedge <richard.pjski2@gmail.com>
2019-04-27 15:25:52 -0400

    Print level 3 page table

--- rustkernelv2/src/main.rs
+++ rustkernelv2/src/main.rs
@@ -30,8 +30,23 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let l4_table = unsafe { active_level_4_table(boot_info.physical_memory_offset) };
 
     for (i, entry) in l4_table.iter().enumerate() {
+        use x86_64::{structures::paging::PageTable, VirtAddr};
+
         if !entry.is_unused() {
             println!("L4 Entry {}: {:?}", i, entry);
+
+            // get the physical address from the entry and convert it
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            // print non-empty entries of the level 3 table
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
